4b02ee6291089392197b77f7fabfe490e3f66452
Zheeeng <hi@zheeeng.me>
2018-04-16 21:13:08 +0800

    Update Omit type for TS version blew 2.8 compatibility

--- index.tsx
+++ index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+export type Diff<T extends string, U extends string> =
+    ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T]
+
+export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>
 
 export interface RemountableProps {
   remount: () => void
