c8a15ce38c9f98b0ae8b8b663464a0b3cdc46593
Zheeeng <hi@zheeeng.me>
2018-05-14 15:09:43 +0800

    Update advanced types

--- index.tsx
+++ index.tsx
@@ -5,16 +5,19 @@ export type Diff<T extends string, U extends string> =
 
 export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>
 
-export interface RemountableProps {
+export interface RemountProps {
   remount: () => void
 }
+
+export type RemountableProps<P extends RemountProps> = Omit<P, keyof RemountProps>
 
 export interface RemountableState {
   count: number,
 }
 
-function remountable <P extends RemountableProps> (WrappedComponent: React.ComponentType<P>) {
-  return class Remountable extends React.Component<Omit<P, 'remount'>> {
+function remountable <P extends RemountProps> (WrappedComponent: React.ComponentType<P>):
+React.ComponentClass<RemountableProps<P>> {
+  return class Remountable extends React.Component<RemountableProps<P>, RemountableState> {
     static displayName: string = `remountable(${
       WrappedComponent.displayName || WrappedComponent.name || 'Component'
     })`
